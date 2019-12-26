f7d842cb5862a92f4c5766d20c6a62138993a2e1
Joey Killam <jhkillam@gmail.com>
2019-12-25 19:53:33 -0600

    edits to layout

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.parallax');
-  var instances = M.Parallax.init(elems);
-})
-
-document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.collapsible');
   var instances = M.Collapsible.init(elems);
 })
