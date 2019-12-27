4f4d53cc31fb36829588ae76aa44219baa7041c1
Joey Killam <jhkillam@gmail.com>
2019-12-27 02:12:31 -0600

    added smooth fade of hero img and favicon

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelector('.sidenav')
-  var instances = M.Sidenav.init(elems)
+  const elems = document.querySelector('.sidenav')
+  const instances = M.Sidenav.init(elems)
 })
 
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.collapsible');
-  var instances = M.Collapsible.init(elems);
+  const elems = document.querySelectorAll('.collapsible');
+  const instances = M.Collapsible.init(elems);
 })
+
+const image = new Image();
+const element = document.getElementsByClassName("hero-img")[0];
+
+image.onload = () => {
+  element.style.backgroundImage = "url('../img/fotis-fotopoulos-DuHKoV44prg-unsplash.jpg')";
+  element.style.animationPlayState = "running";
+}
+image.src = "../img/fotis-fotopoulos-DuHKoV44prg-unsplash.jpg";
+if (image.complete) image.onload();
--- scripts/typed-config.js
+++ scripts/typed-config.js
@@ -1,5 +1,7 @@
-var typedGreeting = new Typed('#typed', {
-  strings: ['He^250llo^500.'],
-  startDelay: 1250,
-  typeSpeed: 150,
-});
+window.onload = () => {
+  var typedGreeting = new Typed('#typed', {
+    strings: ['He^250llo^500.'],
+    startDelay: 1250,
+    typeSpeed: 150,
+  });
+}
